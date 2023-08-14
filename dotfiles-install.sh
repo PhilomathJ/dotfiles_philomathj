32ecd1f7c9780b1ee5f788aef231c555a7e1de71
Jeremy Fairbanks <PhilomathJ@gmail.com>
2023-08-14 13:05:08 -0400

    Update dotfiles-install.sh

--- dotfiles-install.sh
+++ dotfiles-install.sh
@@ -1,21 +1,14 @@
-#!/usr/bin/env bash
-
-DOTFILESDIR = "$HOME/.dotfiles"
-DOTFILESBACKUPDIR = "$HOME/.dotfiles-backup"
-
-# Make directory in which to store any pre-existing dotfiles before overwriting
 mkdir -p $DOTFILESBACKUPDIR
+echo  "Created $DOTFILESDIR and $DOTFILESBACKUPDIR"
 
 # # Make backups of critical files hiding ny errors
-# cp $HOME/.bashrc $DOTFILESBACKUPDIR 2>/dev/null
-# cp $HOME/.bash_aliases $DOTFILESBACKUPDIR 2>/dev/null
-# cp $HOME/.bash_functions $DOTFILESBACKUPDIR 2>/dev/null
-# cp $HOME/.zshrc $DOTFILESBACKUPDIR 2>/dev/null
-# cp $HOME/.zsh_aliases $DOTFILESBACKUPDIR 2>/dev/null
-# cp $HOME/.zsh_functions $DOTFILESBACKUPDIR 2>/dev/null
-
-# create directory for dotfiles bare repository
-mkdir -p $DOTFILESDIR
+cp $HOME/.bashrc $DOTFILESBACKUPDIR 2>/dev/null
+cp $HOME/.bash_aliases $DOTFILESBACKUPDIR 2>/dev/null
+cp $HOME/.bash_functions $DOTFILESBACKUPDIR 2>/dev/null
+cp $HOME/.zshrc $DOTFILESBACKUPDIR 2>/dev/null
+cp $HOME/.zsh_aliases $DOTFILESBACKUPDIR 2>/dev/null
+cp $HOME/.p10k.zsh $DOTFILESBACKUPDIR 2>/dev/null
+echo "Made backup copies of crucial dotfiles in $DOTFILESBACKUPDIR"
 
 # Pull down github dotfiles repository
 git clone --bare git@github.com:PhilomathJ/dotfiles.git $DOTFILEDSIR
