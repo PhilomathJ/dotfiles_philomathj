38b872a037ceebaa5590febfeebda9efc46e4133
Jeremy Fairbanks <PhilomathJ@gmail.com>
2023-12-14 14:18:09 -0500

    Update and rename fix-thorium-pwas.sh to fix-chromium-pwas.sh
    Added sed command for brave-bin to brave browser

--- fix-thorium-pwas.sh
+++
@@ -1,3 +0,0 @@
-# Fixes the Exec path in the PWA *.desktop file
-sed -i 's/opt\/chromium\.org\/thorium\/thorium-browser/opt\/thorium-browser\/thorium-browser/' /home/jeremy/.local/share/applications/thorium-*
-echo "All fixed"
