a9eed906ad56984843cd8efa88ea4b190e6784a2
Jeremy Fairbanks <jsfairbanks@gmail.com>
2025-01-06 16:00:46 -0500

    Delete fix-chromium-pwas.sh

--- fix-chromium-pwas.sh
+++
@@ -1,6 +0,0 @@
-# Fixes the Exec path in the Thorium PWA *.desktop file
-sed -i 's/opt\/chromium\.org\/thorium\/thorium-browser/opt\/thorium-browser\/thorium-browser/' /home/jeremy/.local/share/applications/thorium-*
-
-# Fixes the Exec path in the brave-bin PWA *.desktop file
-sed -i 's/opt\/brave-bin\/brave/usr\/bin\/brave/g' /home/jeremy/.local/share/applications/brave-*
-echo "All fixed"
